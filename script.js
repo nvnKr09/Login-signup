758997f20aa3fb21d4bcbb1d10a1fcc316f94cb8
Naveen Kumar <nvnkr09@gmail.com>
2024-01-08 20:06:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglePassword(inputId, eyeIcon) {
+    const passwordInput = document.getElementById(inputId);
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+}
